0776c3087cdd3bd134f773752486811368d089ee
sadhanapanthi <panthisadhana123@gmail.com>
2023-11-03 11:28:36 +0545

    media and templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import News
 # Create your views here.
+def home(request):
+    first_news=News.objects.first()
+    return render(request, 'home.html')
